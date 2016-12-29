1d233035c3a92c971cefb99a03ca26f690be04c1
Cameron Bergoon <cbergoon@gmail.com>
2016-12-29 13:31:08 -0500

    Adding loadbalancer source

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/cbergoon/load-balancer/proxy"
+	"github.com/cbergoon/load-balancer/registry"
+)
+
+// ServiceRegistry is a local registry of services/versions
+var ServiceRegistry = registry.DefaultRegistry{
+	"service1": {
+		"v1": {
+			"localhost:9091",
+			"localhost:9092",
+		},
+	},
+}
+
+func runLoadBalancer() {
+	go http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		http.Redirect(w, req,
+			"https://localhost:8443"+req.URL.String(),
+			http.StatusMovedPermanently)
+	}))
+
+	http.HandleFunc("/", proxy.NewMultipleHostReverseProxy(ServiceRegistry))
+	http.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "%v\n", ServiceRegistry)
+	})
+
+	log.Fatal(http.ListenAndServeTLS(":8443", "server.crt", "server.key", nil))
+}
+
+func main() {
+	runLoadBalancer()
+}
