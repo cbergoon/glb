d48c06c2d1543259eaa12e94f8acf74523a74558
Cameron Bergoon <cbergoon@gmail.com>
2017-01-17 11:04:44 -0500

    Adding tests to proxy package

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -1,1 +1,30 @@
-package proxy
+package proxy_test
+
+import (
+	"testing"
+	"github.com/cbergoon/glb/registry"
+	"github.com/cbergoon/glb/proxy"
+)
+
+var serviceRegistry = registry.DefaultRegistry{
+	"service1": {
+		"v1": {
+			"localhost:8888",
+		},
+	},
+	"service2": {
+		"v2": {
+			"localhost:7777",
+			"localhost:6666",
+		},
+	},
+}
+
+func TestNewMultipleHostReverseProxy(t *testing.T) {
+	var FALSE = false
+	var ZERO = 0
+	handlerFunc := proxy.NewMultipleHostReverseProxy(serviceRegistry, &FALSE, &ZERO, &FALSE)
+	if handlerFunc == nil {
+		t.Error("Expected handler func got ", handlerFunc)
+	}
+}
