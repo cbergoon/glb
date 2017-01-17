f5df0608f446f688cd8c3e67c87996621af6544f
Cameron Bergoon <cbergoon@gmail.com>
2017-01-17 14:49:20 -0500

    Documentations

--- registry/registry.go
+++ registry/registry.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 )
 
-var lock sync.RWMutex
+var lock sync.RWMutex //Exclusive lock for registry data structure.
 
 var (
 	ErrServiceNotFound       = errors.New("registry: target name/version not found")
@@ -13,14 +13,17 @@ var (
 )
 
 type Registry interface {
-	Add(name, version, endpoint string)
-	Delete(name, version, endpoint string)
-	Lookup(name, version string) ([]string, error)
-	Validate() error
+	Add(name, version, endpoint string) //Adds an entry to registry.
+	Delete(name, version, endpoint string) //Removes an entry from the registry.
+	Lookup(name, version string) ([]string, error) //Retrieves a slice of addresses for specified service/version.
+	Validate() error //Ensures no services contain a reserved word.
 }
 
+//Registry data structure
 type DefaultRegistry map[string]map[string][]string
 
+//Retrieves a slice of addresses for specified service/version. If no address is found
+//ErrServiceNotFound is returned.
 func (r DefaultRegistry) Lookup(name, version string) ([]string, error) {
 	lock.RLock()
 	targets, ok := r[name][version]
@@ -31,6 +34,9 @@ func (r DefaultRegistry) Lookup(name, version string) ([]string, error) {
 	return targets, nil
 }
 
+//Adds an entry to the registry. If the address for an entry exists it will be duplicated.
+//If the service does not exist a new map[string][]string will be created and added to represent
+//the new service. If necessary registry.Lookup can be used to ensure success.
 func (r DefaultRegistry) Add(name, version, endpoint string) {
 	lock.Lock()
 	defer lock.Unlock()
@@ -43,6 +49,8 @@ func (r DefaultRegistry) Add(name, version, endpoint string) {
 	service[version] = append(service[version], endpoint)
 }
 
+//Removes an address entry for a given service/version entry. If necessary the
+//registry.Lookup function can be used to ensure success.
 func (r DefaultRegistry) Delete(name, version, endpoint string) {
 	lock.Lock()
 	defer lock.Unlock()
@@ -62,6 +70,9 @@ begin:
 	}
 }
 
+//Ensures registry adheres to the restrictions set forth by the registry definition. Currently
+//ensures glb keywords are not used in the service/version combination. If this constraint is
+//violated ErrServiceNameNotAllowed is returned.
 func (r DefaultRegistry) Validate() error {
 	_, ok := r["reload"]
 	if ok {
