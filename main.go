da09d0d283ab4a68fe9a9f048e7c9cdbe22cd496
Cameron Bergoon <cbergoon@gmail.com>
2017-01-17 12:08:31 -0500

    Added option to run without HTTPS via configuration

--- main.go
+++ main.go
@@ -11,26 +11,33 @@ import (
 	"github.com/cbergoon/glb/registry"
 )
 
-var ServiceRegistry *registry.DefaultRegistry = &registry.DefaultRegistry{}
-var BasicProxy bool = false
-var IdleConnTimeoutSeconds int = 1
-var DisableKeepAlives bool = false
+const (
+	CONFIG_FILE = "glb.json" //File containing configurataion
+	CERT_FILE = "server.cert" //SSL Certificate
+	KEY_FILE = "server.key" //SSL Key
+)
 
+var ServiceRegistry *registry.DefaultRegistry = &registry.DefaultRegistry{} //Service registry to store service-address mappings.
+var BasicProxy bool = false //Enable single service "default" service/version. Removes requirement of service/version in URL.
+var IdleConnTimeoutSeconds int = 1 //Duration the transport should keep connections alive. Zero imposes no limit.
+var DisableKeepAlives bool = false //Do not keep alive, reconnect on each request.
+
+//Starts load balancer, redirect for HTTPS and, service endpoints.
 func runLoadBalancer(addr, port, sslPort string) {
-
 	//Redirect to HTTPS
-	go http.ListenAndServe(port, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		http.Redirect(w, req,
-			"https://"+addr+sslPort+req.URL.String(),
-			http.StatusMovedPermanently)
-	}))
-
-	//Service Endpoints
+	if sslPort != "" {
+		go http.ListenAndServe(port, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			http.Redirect(w, req,
+				"https://"+addr+sslPort+req.URL.String(),
+				http.StatusMovedPermanently)
+		}))
+	}
+	//GLB Service Endpoints
 	http.HandleFunc("/status", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "%v\n", *ServiceRegistry)
 	})
 	http.HandleFunc("/reload", func(w http.ResponseWriter, req *http.Request) {
-		config, err := ReadParseConfig("glb.conf")
+		config, err := ReadParseConfig(CONFIG_FILE)
 		if err != nil {
 			log.Print(err)
 			os.Exit(-1)
@@ -41,16 +48,18 @@ func runLoadBalancer(addr, port, sslPort string) {
 		DisableKeepAlives = config.DisableKeepAlives
 		fmt.Fprintf(w, "%v\n", *ServiceRegistry)
 	})
-
 	//Proxy Endpoint
 	http.HandleFunc("/", proxy.NewMultipleHostReverseProxy(ServiceRegistry, &BasicProxy, &IdleConnTimeoutSeconds, &DisableKeepAlives))
-
-	log.Fatal(http.ListenAndServeTLS(sslPort, "server.crt", "server.key", nil))
+	if sslPort != "" {
+		log.Fatal(http.ListenAndServeTLS(sslPort, CERT_FILE, KEY_FILE, nil))
+	}else{
+		log.Fatal(http.ListenAndServe(sslPort, nil))
+	}
 }
 
 func main() {
 	//Configure
-	config, err := ReadParseConfig("glb.conf")
+	config, err := ReadParseConfig(CONFIG_FILE)
 	if err != nil {
 		log.Print(err)
 		os.Exit(-1)
